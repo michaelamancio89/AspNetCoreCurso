d6b7456cff999a1c432d3fe97da90ad64fce6570
Michael Amâncio <michael89amancio@gmail.com>
2021-01-27 02:20:35 -0300

    order of middlewares

---
+++ Aula4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var host = WebHost
+           .CreateDefaultBuilder(args)
+           .UseStartup<Startup>()
+           .Build();
+
+           host.Run();
+        }
+    }
+
+    internal class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+          _configuration = configuration;   
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+
+            var ordem = string.Empty;
+
+            app.Use(async (context, next) => {
+
+                ordem += "1";
+                await next.Invoke();
+
+                ordem += "4";
+                await context.Response.WriteAsync($"Ordem: {ordem}");
+            });
+
+            app.Use(async (context, next) => {
+
+                ordem += "2";
+                await next.Invoke();
+
+                ordem += "3";
+                //await context.Response.WriteAsync("Bem Vindo 2");
+            });
+        }
+    }
+}
