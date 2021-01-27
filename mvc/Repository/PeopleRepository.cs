883a8e1a8a48e587ca5700fec6ded3bea5964bef
Michael Amâncio <michael89amancio@gmail.com>
2021-01-27 03:21:58 -0300

    dependency injection

---
+++ mvc/Repository/PeopleRepository.cs
@@ -0,0 +1,18 @@
+namespace mvc.Repository
+{
+    public interface IPeopleRepository {
+
+        string GetNameById(int id);
+    }
+
+    public class PeopleRepository : IPeopleRepository
+    {
+        public PeopleRepository(string connectionString){
+            
+        }
+        public string GetNameById(int id)
+        {
+            return "Michael Amâncio";
+        }
+    }
+}
